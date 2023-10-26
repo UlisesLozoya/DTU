69331b0a08c6b927ac42b7ba177e6e25601ece98
Ulises <uliseslozoya2002@gmail.com>
2023-10-26 06:29:55 +0200

    week 8

--- 02002students-master/cp/ex08/growth_experiment.py
+++ 02002students-master/cp/ex08/growth_experiment.py
@@ -1,6 +1,9 @@
 """Exercise 8.9: Bacteria Growth experiment."""
 import csv
 
+import matplotlib.pyplot as plt
+
+
 def growth_threshold_reached(path: str, threshold: float) -> float:
     """Return the time point at which the growth threshold is reached.
     
@@ -10,3 +13,12 @@ def growth_threshold_reached(path: str, threshold: float) -> float:
     :return: The average time point.
     """
     # TODO: Code has been removed from here. 
+
+
+if __name__ == "__main__":
+    for i in range(160):
+        with open(f'files/experiments/experiment_{i:03d}.csv', 'r') as f:
+            reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+            row = next(csvreader)
+            plt.plot(row)
+    plt.show()
--- 02002students-master/cp/tests/tests_week07.py
+++ 02002students-master/cp/tests/tests_week07.py
@@ -1,69 +1,57 @@
-import cp
 from unitgrade import Report
+import cp
 from unitgrade import UTestCase
 
 
 class Week07HaveEqualLength(UTestCase):
     def test_equal_length_tuples(self):
         from cp.ex07.have_equal_length import have_equal_length
-
         self.assertIs(have_equal_length((1, 2, 3), (4, 5, 6)), True)
 
     def test_unequal_length_tuples(self):
         from cp.ex07.have_equal_length import have_equal_length
-
         self.assertIs(have_equal_length((1, 2, 3), (4, 5)), False)
 
     def test_empty_tuples(self):
         from cp.ex07.have_equal_length import have_equal_length
-
         self.assertIs(have_equal_length((), ()), True)
 
     def test_one_empty_tuple(self):
         from cp.ex07.have_equal_length import have_equal_length
-
         self.assertIs(have_equal_length((1, 2, 3), ()), False)
 
     def test_nested_tuples(self):
         from cp.ex07.have_equal_length import have_equal_length
-
         self.assertIs(have_equal_length((1, (2, 3), 4), (5, (6, 7), 8)), True)
 
 
 class Week07LastDifference(UTestCase):
+
     def test_last_element_difference(self):
         from cp.ex07.last_difference import last_difference
-
-        self.assertEqual(last_difference((10, 7, 8, 20, 30.0), (5, 15, 35.0)), -5.0)
-        self.assertEqual(last_difference((1, 2, 3.0), (4, 5, 6.0)), -3.0)
+        self.assertEqual(last_difference((10, 7, 8, 20, 30.), (5, 15, 35.)), -5.0)
+        self.assertEqual(last_difference((1, 2, 3.), (4, 5, 6.)), -3.0)
 
 
 class Week07MultipleValues(UTestCase):
+
     def test_returning_multiple_values(self):
         from cp.ex07.returning_multiple_values import returning_multiple_values
-
-        self.assertEqual(
-            returning_multiple_values([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5),
-            ([6, 7, 8, 9, 10], 1),
-        )
-        self.assertEqual(
-            returning_multiple_values([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10), ([], 1)
-        )
-        self.assertEqual(
-            returning_multiple_values([-1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
-            ([2, 3, 4, 5, 6, 7, 8, 9, 10], -1),
-        )
+        self.assertEqual(returning_multiple_values([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5), ([6, 7, 8, 9, 10], 1))
+        self.assertEqual(returning_multiple_values([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10), ([], 1))
+        self.assertEqual(returning_multiple_values([-1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
+                         ([2, 3, 4, 5, 6, 7, 8, 9, 10], -1))
 
 
 class Week07BoxPacking(UTestCase):
     def assertTupleAlmostEqual(self, first, second, places=None, msg=None, delta=None):
+        from cp.ex07.box_packing import box_packing
         self.assertEqual(len(first), len(second))
         for a, b in zip(first, second):
             self.assertAlmostEqual(a, b, places, msg, delta)
 
     def test_box_packing(self):
         from cp.ex07.box_packing import box_packing
-
         self.assertTupleAlmostEqual(box_packing((2, 5), (4, 2.1)), (0, 2.9))
         self.assertTupleAlmostEqual(box_packing((4, 2.1), (5, 2)), (0, 0.1))
         self.assertTupleAlmostEqual(box_packing((4, 2.1), (5, 5)), (0, 0))
@@ -75,13 +63,12 @@ class Week07BoxPacking(UTestCase):
 class Week07Hue(UTestCase):
     def test_hue(self):
         from cp.ex07.rgb_to_hue import rgb_to_hue
-
         self.assertEqual(rgb_to_hue((0.6, 0.2, 0.3)), 345.0)
         self.assertEqual(rgb_to_hue((0.3, 0.6, 0.2)), 105.0)
         self.assertEqual(rgb_to_hue((0.1, 0.1, 0.2)), 240.0)
         self.assertEqual(rgb_to_hue((0.2, 0.25, 0.3)), 210.0)
-        self.assertEqual(rgb_to_hue((0.9, 0.9, 0.3)), 60.0)
-        self.assertEqual(rgb_to_hue((0.6, 0.8, 0.8)), 180.0)
+        self.assertEqual(rgb_to_hue((0.9, 0.9, 0.3)), 60.)
+        self.assertEqual(rgb_to_hue((0.6, 0.8, 0.8)), 180.)
         self.assertEqual(rgb_to_hue((1.0, 0.1, 0.1)), 0.0)
         self.assertEqual(rgb_to_hue((0.7, 0.6, 0.7)), 300.0)
         self.assertEqual(rgb_to_hue((0.5, 0.1, 0.6)), 288.0)
@@ -90,7 +77,6 @@ class Week07Hue(UTestCase):
 class Week07CodeShift(UTestCase):
     def test_code_shift(self):
         from cp.ex07.code_shift import code_shift
-
         turn = (1, 2, 3, 4)
         result = code_shift((5, 3, 7, 2), turn)
         self.assertEqual(result, (6, 5, 0, 6))
@@ -105,61 +91,41 @@ class Week07CodeShift(UTestCase):
 class Week07Morse(UTestCase):
     def test_morse(self):
         from cp.ex07.morse_to_text import morse_to_text
-
-        self.assertEqual(
-            morse_to_text(
-                "-.. --- - ...  .- -. -..  -.. .- ... .... . ...  - . .-.. .-..  .- -. -.-. .. . -. -  - .- .-.. . ..."
-            ),
-            "DOTS AND DASHES TELL ANCIENT TALES",
-        )
-        self.assertEqual(
-            morse_to_text("..  --. ---  - ---  - .... .  -.. . -. - .. ... -"),
-            "I GO TO THE DENTIST",
-        )
-        self.assertEqual(
-            morse_to_text("- --- -.. .- -.--  .-- .- ...  -.-. .-.. . .- .-."),
-            "TODAY WAS CLEAR",
-        )
-        self.assertEqual(
-            morse_to_text(
-                "- .... .  -- .- --. .. -.-.  --- ..-.  - .... .  ... ..- -."
-            ),
-            "THE MAGIC OF THE SUN",
-        )
-        self.assertEqual(
-            morse_to_text("- .... .  .--- --- -.--  .. -.  -- -.--  .... . .- .-. -"),
-            "THE JOY IN MY HEART",
-        )
+        self.assertEqual(morse_to_text(
+            "-.. --- - ...  .- -. -..  -.. .- ... .... . ...  - . .-.. .-..  .- -. -.-. .. . -. -  - .- .-.. . ..."),
+                         "DOTS AND DASHES TELL ANCIENT TALES")
+        self.assertEqual(morse_to_text("..  --. ---  - ---  - .... .  -.. . -. - .. ... -"), "I GO TO THE DENTIST")
+        self.assertEqual(morse_to_text("- --- -.. .- -.--  .-- .- ...  -.-. .-.. . .- .-."), "TODAY WAS CLEAR")
+        self.assertEqual(morse_to_text("- .... .  -- .- --. .. -.-.  --- ..-.  - .... .  ... ..- -."),
+                         "THE MAGIC OF THE SUN")
+        self.assertEqual(morse_to_text("- .... .  .--- --- -.--  .. -.  -- -.--  .... . .- .-. -"),
+                         "THE JOY IN MY HEART")
 
 
 class Week07AstronomicalSeason(UTestCase):
     def test_winter(self):
         from cp.ex07.astronomical_season import astronomical_season
-
-        self.assertEqual(astronomical_season((21, "dec")), "winter")
-        self.assertEqual(astronomical_season((15, "feb")), "winter")
-        self.assertEqual(astronomical_season((19, "mar")), "winter")
+        self.assertEqual(astronomical_season((21, 'dec')), 'winter')
+        self.assertEqual(astronomical_season((15, 'feb')), 'winter')
+        self.assertEqual(astronomical_season((19, 'mar')), 'winter')
 
     def test_spring(self):
         from cp.ex07.astronomical_season import astronomical_season
-
-        self.assertEqual(astronomical_season((20, "mar")), "spring")
-        self.assertEqual(astronomical_season((10, "apr")), "spring")
-        self.assertEqual(astronomical_season((20, "jun")), "spring")
+        self.assertEqual(astronomical_season((20, 'mar')), 'spring')
+        self.assertEqual(astronomical_season((10, 'apr')), 'spring')
+        self.assertEqual(astronomical_season((20, 'jun')), 'spring')
 
     def test_summer(self):
         from cp.ex07.astronomical_season import astronomical_season
-
-        self.assertEqual(astronomical_season((21, "jun")), "summer")
-        self.assertEqual(astronomical_season((5, "jul")), "summer")
-        self.assertEqual(astronomical_season((22, "sep")), "summer")
+        self.assertEqual(astronomical_season((21, 'jun')), 'summer')
+        self.assertEqual(astronomical_season((5, 'jul')), 'summer')
+        self.assertEqual(astronomical_season((22, 'sep')), 'summer')
 
     def test_autumn(self):
         from cp.ex07.astronomical_season import astronomical_season
-
-        self.assertEqual(astronomical_season((23, "sep")), "autumn")
-        self.assertEqual(astronomical_season((10, "oct")), "autumn")
-        self.assertEqual(astronomical_season((20, "dec")), "autumn")
+        self.assertEqual(astronomical_season((23, 'sep')), 'autumn')
+        self.assertEqual(astronomical_season((10, 'oct')), 'autumn')
+        self.assertEqual(astronomical_season((20, 'dec')), 'autumn')
 
 
 class Week07Tests(Report):  # 240 total.
@@ -168,19 +134,18 @@ class Week07Tests(Report):  # 240 total.
     # url = "https://gitlab.compute.dtu.dk/cp/02002students/-/blob/master/cp/tests"
     pack_imports = [cp]
     individual_imports = []
-    questions = [
-        (Week07HaveEqualLength, 10),
-        (Week07LastDifference, 10),
-        (Week07MultipleValues, 10),
-        (Week07BoxPacking, 10),
-        (Week07Hue, 10),
-        (Week07CodeShift, 10),
-        (Week07Morse, 10),
-        (Week07AstronomicalSeason, 10),
-    ]
+    questions = [(Week07HaveEqualLength, 10),
+                 (Week07LastDifference, 10),
+                 (Week07MultipleValues, 10),
+                 (Week07BoxPacking, 10),
+                 (Week07Hue, 10),
+                 (Week07CodeShift, 10),
+                 (Week07Morse, 10),
+                 (Week07AstronomicalSeason, 10)
+                 ]
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from unitgrade import evaluate_report_student
 
     evaluate_report_student(Week07Tests())
