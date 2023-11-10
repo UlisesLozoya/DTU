66f07ef5ee7829ca73e6279d457c5d058bbf58da
Ulises <uliseslozoya2002@gmail.com>
2023-11-10 22:07:03 +0100

    week 10 classes

--- 02002students-master/cp/ex10/shopping_cart.py
+++ 02002students-master/cp/ex10/shopping_cart.py
@@ -77,33 +77,3 @@ class Inventory:
             return True
         else:
             return False
-
-
-if __name__ == "__main__":
-    inventory = Inventory()
-    item1 = Item("Laptop", 10, 800)
-    item2 = Item("Phone", 20, 400)
-    item3 = Item("Tablet", 15, 300)
-    item4 = Item("Laptop", 2, 850)
-    item5 = Item("Phone", 5, 420)
-
-    inventory.add_item(item1)
-    inventory.add_item(item2)
-    inventory.add_item(item3)
-
-    inventory2 = Inventory()
-    inventory2.add_item(item2)
-    inventory2.add_item(item4)
-    inventory2.add_item(item5)
-
-    inventory3 = Inventory()
-    inventory3.add_item(item3)
-    inventory3.add_item(item4)
-    inventory3.add_item(item5)
-
-    print(inventory.listitems)
-    print(inventory2.listitems)
-    print(inventory3.listitems)
-    inventory.calculate_total_value()
-    inventory2.calculate_total_value()
-    inventory3.calculate_total_value()
